8c3b9ab05a0730d67839e904790029615b741869
Oliver Beach <17b23772@marling.school>
2021-01-13 10:17:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# 
+print("Welcome to Copington Adventure ThemePark")
+print("Ticket prices Adult- £20")
+print("Ticket prices Child(Below 14)- £12")
+print("Ticket prices Senior(60+) - £11")
+print("Wrist Band- £20")
+print("Parking Pass- Free")
+
+print("\n")
+adult_tickets = 0
+while adult_tickets == 0:
+  print("Would you like any adult tickets?")
+  print("Enter how many tickets you want in the section below:")
+  adult_tickets = 0 + int(input())
+print("\n")
+child_tickets = 0
+while child_tickets == 0:
+  print("Would you like any Child tickets?")
+  print("Enter how many tickets you want in the section below:")
+  child_tickets = 0 + int(input())
+
+
